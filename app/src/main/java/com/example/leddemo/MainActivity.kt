c8590777e26e679a92946780ab19332ba6e56104
彭俊 <pengjun@pengjundeMacBook-Pro.local>
2019-05-03 23:23:20 +0800

    v3, add natave java

--- app/src/main/java/com/example/leddemo/MainActivity.kt
+++ app/src/main/java/com/example/leddemo/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import com.example.hardlibrary.*;
 
 import kotlinx.android.synthetic.main.activity_main.*
 
